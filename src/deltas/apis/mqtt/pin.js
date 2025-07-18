9f03e123c991d14ac9d530638dd2eddfcca38db7
CHORU-OFFICIAL <112081055+ChoruOfficial@users.noreply.github.com>
2025-07-18 22:37:10 +0800

    Update pin.js

--- src/deltas/apis/mqtt/pin.js
+++ src/deltas/apis/mqtt/pin.js
@@ -1,7 +1,6 @@
 "use strict";
 
-require("module-alias/register");
-const utils = require("@utils");
+const utils = require('../../../utils'); 
 const fs = require("fs");
 const path = require("path");
 const _ = require("lodash");
