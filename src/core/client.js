4a84d831d6cfc2201a4980d1870b567f1d82924d
CHORU-OFFICIAL <112081055+ChoruOfficial@users.noreply.github.com>
2025-07-18 22:40:58 +0800

    Update client.js

--- src/core/client.js
+++ src/core/client.js
@@ -1,7 +1,6 @@
 "use strict";
 
-require('module-alias/register');
-const utils = require("@utils");
+const utils = require("../utils");
 const setOptionsModel = require('./models/setOptions');
 const buildAPIModel = require('./models/buildAPI');
 const loginHelperModel = require('./models/loginHelper');
