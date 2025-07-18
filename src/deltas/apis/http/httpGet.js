ad9df0593a76e8293eafb974c0c46fb19371834b
CHORU-OFFICIAL <112081055+ChoruOfficial@users.noreply.github.com>
2025-07-18 22:52:24 +0800

    Update httpGet.js

--- src/deltas/apis/http/httpGet.js
+++ src/deltas/apis/http/httpGet.js
@@ -1,7 +1,6 @@
 "use strict";
 
-require('module-alias/register');
-const utils = require("@utils");
+const utils = require('../../../utils');
 
 module.exports = function (defaultFuncs, api, ctx) {
   return function httpGet(url, form, customHeader, callback, notAPI) {
