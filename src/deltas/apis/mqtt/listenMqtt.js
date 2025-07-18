ef64e9cc7a852d32a5d66c39b9558816a001827c
CHORU-OFFICIAL <112081055+ChoruOfficial@users.noreply.github.com>
2025-07-18 22:35:37 +0800

    Update listenMqtt.js

--- src/deltas/apis/mqtt/listenMqtt.js
+++ src/deltas/apis/mqtt/listenMqtt.js
@@ -1,7 +1,6 @@
 "use strict";
 
-require('module-alias/register');
-const utils = require("@utils");
+const utils = require('../../../utils'); // Changed from @utils to relative path
 const mqtt = require('mqtt');
 const websocket = require('websocket-stream');
 const HttpsProxyAgent = require('https-proxy-agent');
