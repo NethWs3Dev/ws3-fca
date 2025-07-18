4ff4ca6babae074d6bf769410681c7182b42edb6
CHORU-OFFICIAL <112081055+ChoruOfficial@users.noreply.github.com>
2025-07-18 22:53:21 +0800

    Update addExternalModule.js

--- src/deltas/apis/extra/addExternalModule.js
+++ src/deltas/apis/extra/addExternalModule.js
@@ -1,6 +1,5 @@
 "use strict";
-require('module-alias/register');
-const utils = require("@utils");
+const utils = require('../../../utils');
 
 module.exports = function (defaultFuncs, api, ctx) {
   return function addExternalModule(moduleObj) {
