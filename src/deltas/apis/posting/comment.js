49a9b6ae323ecc57f0651f57b0e6fb677bf10d21
CHORU-OFFICIAL <112081055+ChoruOfficial@users.noreply.github.com>
2025-07-18 22:46:57 +0800

    Update comment.js

--- src/deltas/apis/posting/comment.js
+++ src/deltas/apis/posting/comment.js
@@ -1,7 +1,6 @@
 'use strict';
 
-require('module-alias/register');
-const utils = require("@utils");
+const utils = require('../../../utils');
 
 /**
  * Handles the upload of attachments (images/videos) for a comment.
