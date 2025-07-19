dc7b3f940e3e4a6bb37814a554810be63e9693f2
CHORU-OFFICIAL <112081055+ChoruOfficial@users.noreply.github.com>
2025-07-19 16:43:20 +0800

    Update nickname.js

--- src/deltas/apis/messaging/nickname.js
+++ src/deltas/apis/messaging/nickname.js
@@ -12,41 +12,72 @@ module.exports = function (defaultFuncs, api, ctx) {
    * @param {string} threadID The ID of the thread.
    * @param {string} participantID The ID of the participant whose nickname will be changed. Defaults to the current user's ID if not provided or a function.
    * @param {Function} [callback] Optional callback function to be invoked upon completion.
-   * @returns {Promise<void>} A promise that resolves on success or rejects on error.
+   * @param {string} [initiatorID] The ID of the user who initiated the nickname change (e.g., from event.senderID).
+   * @returns {Promise<object>} A promise that resolves with a structured event object on success or rejects on error.
    */
-  return function setNickname(nickname, threadID, participantID, callback) { // This is the function that will be attached to api.setNickname
-    let resolveFunc = function () {};
-    let rejectFunc = function () {};
-    const returnPromise = new Promise(function (resolve, reject) {
-      resolveFunc = resolve;
-      rejectFunc = reject;
+  return function setNickname(nickname, threadID, participantID, callback, initiatorID) {
+    let _callback;
+    let _initiatorID;
+
+    let _resolvePromise;
+    let _rejectPromise;
+    const returnPromise = new Promise((resolve, reject) => {
+        _resolvePromise = resolve;
+        _rejectPromise = reject;
     });
 
-    if (utils.getType(threadID) === "Function" || utils.getType(threadID) === "AsyncFunction") {
-      callback = threadID;
-      threadID = null;
+    if (utils.getType(callback) === "Function" || utils.getType(callback) === "AsyncFunction") {
+        _callback = callback;
+        _initiatorID = initiatorID;
+    } else if (utils.getType(threadID) === "Function" || utils.getType(threadID) === "AsyncFunction") {
+        _callback = threadID;
+        threadID = null;
+        _initiatorID = callback;
+    } else if (utils.getType(participantID) === "Function" || utils.getType(participantID) === "AsyncFunction") {
+        _callback = participantID;
+        participantID = ctx.userID;
+        _initiatorID = callback;
     }
-    if (utils.getType(participantID) === "Function" || utils.getType(participantID) === "AsyncFunction") {
-      callback = participantID;
-      participantID = ctx.userID;
+    else if (utils.getType(callback) === "string") {
+        _initiatorID = callback;
+        _callback = undefined;
+    } else {
+        _callback = undefined;
+        _initiatorID = undefined;
     }
-    if (utils.getType(nickname) === "Function" || utils.getType(nickname) === "AsyncFunction") {
-      callback = nickname;
-      nickname = "";
+
+    if (!_callback) {
+      _callback = function (__err, __data) {
+        if (__err) _rejectPromise(__err);
+        else _resolvePromise(__data);
+      };
+    } else {
+      const originalCallback = _callback;
+      _callback = function(__err, __data) {
+        if (__err) {
+          originalCallback(__err);
+          _rejectPromise(__err);
+        } else {
+          originalCallback(null, __data);
+          _resolvePromise(__data);
+        }
+      };
     }
+
+    _initiatorID = _initiatorID || ctx.userID;
 
     threadID = threadID || ctx.threadID;
     participantID = participantID || ctx.userID;
 
-    if (!callback) {
-      callback = function (err, data) {
-        if (err) return rejectFunc(err);
-        resolveFunc(data);
-      };
+    if (!threadID) {
+      return _callback(new Error("threadID is required to set a nickname."));
+    }
+    if (typeof nickname !== 'string') {
+      return _callback(new Error("nickname must be a string."));
     }
 
     if (!ctx.mqttClient) {
-      return callback({ error: "Not connected to MQTT" });
+      return _callback(new Error("Not connected to MQTT"));
     }
 
     ctx.wsReqNumber += 1;
@@ -79,12 +110,21 @@ module.exports = function (defaultFuncs, api, ctx) {
     };
     context.payload = JSON.stringify(context.payload);
     
-ctx.mqttClient.publish('/ls_req', JSON.stringify(context), { qos: 1, retain: false }, (err) => {
+    ctx.mqttClient.publish('/ls_req', JSON.stringify(context), { qos: 1, retain: false }, (err) => {
       if (err) {
-        utils.error("setNickname", err);
-        return callback(err);
+        return _callback(new Error(`MQTT publish failed for setNickname: ${err.message || err}`));
       }
-      callback(null);
+      
+      const nicknameChangeEvent = {
+        type: "thread_nickname_update", 
+        threadID: threadID,
+        participantID: participantID,
+        newNickname: nickname,
+        senderID: _initiatorID,
+        BotID: ctx.userID,
+        timestamp: Date.now(),
+      };
+      _callback(null, nicknameChangeEvent);
     });
 
     return returnPromise;
